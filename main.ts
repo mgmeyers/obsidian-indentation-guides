238174be6095bd894b467fa50df3504cd63d9e6a
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-12-28 15:13:38 -0800

    Initial commit

--- main.ts
+++ main.ts
@@ -1,137 +1,214 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import {
+  Decoration,
+  DecorationSet,
+  EditorView,
+  MatchDecorator,
+  ViewPlugin,
+  ViewUpdate,
+} from "@codemirror/view";
+import { StateField } from "@codemirror/state";
+import { RangeSetBuilder } from "@codemirror/rangeset";
+import { Line } from "@codemirror/text";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface RelationshipLinesSettings {
+  showActiveIndentationGroup: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: RelationshipLinesSettings = {
+  showActiveIndentationGroup: true,
+};
+
+const tabDecoration = (getSettings: () => RelationshipLinesSettings) =>
+  ViewPlugin.fromClass(
+    class {
+      decorator: MatchDecorator;
+      decorations: DecorationSet = Decoration.none;
+
+      constructor(public view: EditorView) {
+        this.decorator = new MatchDecorator({
+          regexp: new RegExp(`(?:\t| {${view.state.tabSize}})`, "g"),
+          decoration: (match, view) => {
+            if (!getSettings().showActiveIndentationGroup) {
+              return Decoration.mark({
+                class: `cm-tab`,
+              });
+            }
+
+            const currentIndent = view.state.field(activeIndentField);
+
+            return Decoration.mark({
+              class: `cm-tab${
+                currentIndent - 1 === match.index
+                  ? " cm-indent-group-level"
+                  : ""
+              }`,
+            });
+          },
+        });
+
+        this.decorations = this.decorator.createDeco(view);
+      }
+
+      update(update: ViewUpdate) {
+        if (!getSettings().showActiveIndentationGroup) {
+          this.decorations = this.decorator.updateDeco(
+            update,
+            this.decorations
+          );
+        } else {
+          this.decorations = this.decorator.createDeco(update.view);
+        }
+      }
+    },
+    {
+      decorations: (v) => v.decorations,
+    }
+  );
+
+function getLineIndent(tabSize: number, line: Line) {
+  const match = line.text.match(new RegExp(`^((?:\t| {${tabSize}})+)`));
+
+  if (!match) return 0;
+
+  return match[1].split(new RegExp(`(?:\t| {${tabSize}})`)).length - 1;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const indentationGroupDecoration = Decoration.line({
+  attributes: { class: "cm-indent-group" },
+});
 
-	async onload() {
-		await this.loadSettings();
+function tagIndentationGroup(view: EditorView) {
+  const builder = new RangeSetBuilder<Decoration>();
+  const state = view.state;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  if (!state.selection?.main) return builder.finish();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+  const currentLine = state.doc.lineAt(state.selection.main.from);
+  const currentIndent = view.state.field(activeIndentField);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  if (currentIndent === 0) return builder.finish();
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+  const indentationGroup: Line[] = [currentLine];
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  let from: number = currentLine.from;
+  let to: number = currentLine.to;
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  while (from > 0) {
+    const prevLine = state.doc.lineAt(from - 1);
+    const prevIndent = getLineIndent(state.tabSize, prevLine);
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    if (prevIndent >= currentIndent) {
+      indentationGroup.push(prevLine);
+      from = prevLine.from;
+    } else {
+      break;
+    }
+  }
 
-	onunload() {
+  while (to < state.doc.length - 1) {
+    const nextLine = state.doc.lineAt(to + 1);
+    const nextIndent = getLineIndent(state.tabSize, nextLine);
 
-	}
+    if (nextIndent >= currentIndent) {
+      indentationGroup.push(nextLine);
+      to = nextLine.to;
+    } else {
+      break;
+    }
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  indentationGroup
+    .sort((a, b) => a.from - b.from)
+    .forEach((line) => {
+      builder.add(line.from, line.from, indentationGroupDecoration);
+    });
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  return builder.finish();
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+const indentationGroup = ViewPlugin.fromClass(
+  class {
+    decorations: DecorationSet;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(view: EditorView) {
+      this.decorations = tagIndentationGroup(view);
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    update(update: ViewUpdate) {
+      this.decorations = tagIndentationGroup(update.view);
+    }
+  },
+  {
+    decorations: (v) => v.decorations,
+  }
+);
+
+const activeIndentField = StateField.define<number>({
+  create(state) {
+    if (!state.selection?.main) return 0;
+    return getLineIndent(
+      state.tabSize,
+      state.doc.lineAt(state.selection.main.from)
+    );
+  },
+  update(_, tr) {
+    const state = tr.state;
+    return getLineIndent(
+      state.tabSize,
+      state.doc.lineAt(state.selection.main.from)
+    );
+  },
+});
+
+export default class RelationshipLines extends Plugin {
+  settings: RelationshipLinesSettings;
+
+  async onload() {
+    await this.loadSettings();
+    this.addSettingTab(new RelationshipLinesSettingsTab(this.app, this));
+    this.registerEditorExtension([
+      activeIndentField,
+      indentationGroup,
+      tabDecoration(() => this.settings),
+    ]);
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class RelationshipLinesSettingsTab extends PluginSettingTab {
+  plugin: RelationshipLines;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: RelationshipLines) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    let { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName("Highlight active indentation group")
+      .addToggle((toggle) => {
+        toggle
+          .setValue(
+            this.plugin.settings.showActiveIndentationGroup === undefined
+              ? true
+              : this.plugin.settings.showActiveIndentationGroup
+          )
+          .onChange(async (value) => {
+            this.plugin.settings.showActiveIndentationGroup = value;
+            await this.plugin.saveSettings();
+          });
+      });
+  }
 }
